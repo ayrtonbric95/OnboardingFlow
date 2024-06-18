725cb66edef5316e2a14bb1e0025a0ab62049cf3
Ayrton Briceno <iqbriceno@gmail.com>
2024-06-17 23:00:47 -0400

    Initial Commit

---
+++ OnboardingFlow/OnboardingFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingFlowApp.swift
+//  OnboardingFlow
+//
+//  Created by Ayrton Briceno on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
