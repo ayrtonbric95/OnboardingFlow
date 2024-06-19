7392ff5cf1236ebea5cced264251d33e23d227ce
Ayrton Briceno <iqbriceno@gmail.com>
2024-06-18 23:18:17 -0400

    Initial commit.

---
+++ OnboardingFlow/WelcomePage.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomePage.swift
+//  OnboardingFlow
+//
+//  Created by Ayrton Briceno on 6/18/24.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
